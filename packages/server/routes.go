b7c36c5a53730ead2f16bf6f7cc61680bece27e6
Clayton Herbst <herbsca1@gmail.com>
2024-04-24 15:28:44 +1000

    fix: successfully connecting to mongodb instance

--- packages/server/routes.go
+++ packages/server/routes.go
@@ -3,18 +3,28 @@ package main
 import (
 	"github.com/cherbie/player-cms/internal/config"
 	"github.com/cherbie/player-cms/internal/controllers"
+	"github.com/cherbie/player-cms/internal/crud"
+	"github.com/cherbie/player-cms/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(engine *gin.Engine) {
-	connectionPool := config.GetConnectionPoolSingleton()
+func setupRoutes(engine *gin.Engine) {
+	playerService := newPlayerServiceSingleton()
 
 	healthController := controllers.NewHealthController()
-	playerController, err := controllers.NewPlayerController(connectionPool)
+	playerController := controllers.NewPlayerController(playerService)
+
+	engine.GET("/health", controllers.HandlerWrapper(healthController.GetHealth))
+	engine.GET("/player", controllers.HandlerWrapper(playerController.GetPlayer))
+}
+
+func newPlayerServiceSingleton() service.PlayerService {
+	connectionPool := config.GetConnectionPoolSingleton()
+	connection, err := connectionPool.Get()
 	if err != nil {
 		panic(err)
 	}
 
-	engine.GET("/health", controllers.HandlerWrapper(healthController.GetHealth))
-	engine.GET("/player", controllers.HandlerWrapper(playerController.GetPlayer))
+	collection := crud.NewCollection(connection.Database("player-cms").Collection("players"))
+	return service.NewPlayerService(collection)
 }
